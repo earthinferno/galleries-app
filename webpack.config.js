9a89614c2f903cd275f62df1fe13b1826be7c840
gs <gstechtastic@outlook.com>
2018-08-27 17:45:30 +0100

    Added webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
     entry: './src/js/app.js',
@@ -6,5 +7,19 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
         publicPath: '/dist'
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        //...
+    ]
 }
