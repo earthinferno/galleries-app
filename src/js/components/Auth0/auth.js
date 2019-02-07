2b205005cc96e5912c73a7ccd4ea4d5fe6ff23b3
Graham Small <graham_small@live.co.uk>
2019-02-07 15:53:32 +0000

    Updated

--- src/js/components/Auth0/auth.js
+++ src/js/components/Auth0/auth.js
@@ -49,8 +49,7 @@ export default class Auth {
           resolve();
         });
       })      
-      promise.then(successCallback);
-      promise.then(err => alert("failure" + err.error));
+      promise.then(successCallback, err => alert("failure" + err.error));
       console.log("handleAuthentication");
       return promise;
   }
